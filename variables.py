e72de10db344cd4fe766720fdae072aefa2d7cdb
chandupraka5h <chandu.kprakash@gmail.com>
2019-11-07 14:43:45 +0530

    first time

---
+++ variables.py
@@ -0,0 +1,18 @@
+variable1 = 10
+variable1 = 20
+# runs from top
+print(variable1)
+
+full_name = "john smith"
+age = 20
+is_new = True
+
+
+
+name = input("what is your name ? ")
+
+print("hi ", name)
+
+color = input("what is your favourite color ? ")
+
+print(name + " likes " + color + " color ")
