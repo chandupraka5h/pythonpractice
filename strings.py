01270bcb9c8b142644cc6aa3a4b7b92253f0ee45
chandupraka5h <chandu.kprakash@gmail.com>
2019-11-07 17:55:29 +0530

    string quotes

--- strings.py
+++ strings.py
@@ -1,1 +1,18 @@
 
+course = "'python' for beginners"
+print(course)
+#single and double quotes
+courses = 'python for "beginners"'
+print(courses)
+
+#triple quotes
+thanking = '''
+
+Hi
+
+Thanking for teaching me python
+
+thank you
+
+'''
+print(thanking)
