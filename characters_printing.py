8f3268f9be7d82dbaae187fe7bb3aaeb79ac846f
chandupraka5h <chandu.kprakash@gmail.com>
2019-11-08 11:43:51 +0530

    characters printing

---
+++ characters_printing.py
@@ -0,0 +1,26 @@
+course = 'python for beginners'
+print(course[0]) 
+#only printing first letter from index
+#or
+
+print(course[-1]) 
+#only printing last letter from index
+#or
+
+print(course[0:5])
+#printing from 0 to 5 character from index
+#or
+
+print(course[2:])
+#or leaving first 3 characters from index and printing all characters
+
+print(course[:5])
+#or printing characters for 0 to 5 from index and leaving all other letters
+
+#or
+
+another = course[:]
+print(another)
+
+name = 'instabee'
+print(name[1:-1])
