76ba1cb4421b6418ae367cf8eb0d1ef8cfec882d
chandupraka5h <chandu.kprakash@gmail.com>
2019-11-08 13:02:16 +0530

    done

---
+++ find_character_index.py
@@ -0,0 +1,17 @@
+course = "python for beginners"
+course_find = course.find('p')
+print(course_find)
+
+#or
+
+name = input("enter your name ")
+
+print(name)
+
+name_character = input("enter the character ")
+
+print("entered character is ", name_character)
+
+name_find = name.find(name_character)
+
+print("current character position is ", name_find)
