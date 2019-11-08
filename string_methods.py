020b4e84e99a7b902db5fb9776dbfed5b8af12c2
chandupraka5h <chandu.kprakash@gmail.com>
2019-11-08 12:24:48 +0530

    sting

---
+++ string_methods.py
@@ -0,0 +1,11 @@
+course = 'python for beginners'
+length = len(course)
+print(length)
+
+#or
+
+name = input('name please ')
+name_length = len(name)
+print(str(name_length) + " characters")
+print(name.upper())
+
